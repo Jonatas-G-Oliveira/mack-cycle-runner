e3d153cea2e429cc8a490760a26bd2fd3ae5a005
Jonatas-G-Oliveira <jonatas11oliveira@live.com>
2025-10-23 23:09:11 -0300

    ✨ feat : add hitbox para os discos

--- jogo/core/src/main/java/com/jonatas/game/Main.java
+++ jogo/core/src/main/java/com/jonatas/game/Main.java
@@ -2,19 +2,28 @@ package com.jonatas.game;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class Main implements ApplicationListener {
     Texture fundoExemplo;
     Texture personagemPrincipal;
+    Texture disco;
+
+    // ----- Lógica disco
+    Array<Sprite> vetorDiscos;
+    Array<Integer> esteira;
+    float discoTimer;
+    Rectangle discoRectangle;
+    Rectangle PersonagemRectangle;
 
     Sprite personagemPrincipalSprite;
     Sprite fundoExemploSprite; 
@@ -31,17 +40,26 @@ public class Main implements ApplicationListener {
         // ------ Carregando imagens
         fundoExemplo = new Texture("fundo_ex.jpg");
         personagemPrincipal = new Texture("treecko.png");
-
+        disco = new Texture("disco.jpg");
+    
         // ------ Definindo Sprites
         personagemPrincipalSprite = new Sprite(personagemPrincipal);
-        personagemPrincipalSprite.setSize(1, 1);
-
+        personagemPrincipalSprite.setSize(2, 2);
+        
         // ------ Iniciando SpriteBatch
         spriteBatch = new SpriteBatch();
-        viewport = new FitViewport(8, 5);
+        viewport = new FitViewport(12, 8);
 
         //Capturando informações de click
         touchPos = new Vector2();
+
+        //Lógica para os discos
+        vetorDiscos = new Array<>();
+        esteira = new Array<>();
+        esteira.add(1);
+        esteira.add(4);
+        discoRectangle = new Rectangle();
+        PersonagemRectangle = new Rectangle();
     }
 
     @Override
@@ -56,46 +74,90 @@ public class Main implements ApplicationListener {
         draw();
     }
 
-    //Tudo que é de clicar,de entrada a gente ve por aqui
+    private void criarDiscos(){
+      
+        float discoWidth = 2;
+        float discoHeight = 2;
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        Sprite discoSprite = new Sprite(disco);
+        discoSprite.setSize(discoWidth, discoHeight);
+        
+        //Escolhendo a esteira que o disco vem
+        int laneEscolhida = MathUtils.random(0, 1);
+        // discoSprite.setPosition(worldWidth - discoWidth - 2, esteira.get(laneEscolhida));
+        //Clamp no disco que eu criei
+        discoSprite.setY(esteira.get(laneEscolhida));
+        discoSprite.setX(worldWidth - discoWidth);
+        vetorDiscos.add(discoSprite);
+        
+    }
+
     public void input(){
-        float velocidade = .5f;
+        float velocidade = 3f;
         float delta = Gdx.graphics.getDeltaTime(); //rodar em qualquer lugar
 
 
         if(Gdx.input.justTouched()){
             touchPos.set(Gdx.input.getX(), Gdx.input.getY()); //Pegando informações de onde foi clicado>>>>>
             if (inverterPosicao) {
-                personagemPrincipalSprite.translateY(velocidade * delta * 150);
+                personagemPrincipalSprite.translateY(velocidade);
             }else{
-                personagemPrincipalSprite.translateY(-velocidade * delta* 100);
+                personagemPrincipalSprite.translateY(-velocidade);
             }
             inverterPosicao = !inverterPosicao;
         }
      
     }
 
-    //Tudo que foi
     public void logic(){
+        //Delta só é bom pra objeto continuos
+        float delta = Gdx.graphics.getDeltaTime();
         float worldWidth = viewport.getWorldWidth();
         float worldHeight = viewport.getWorldHeight();
-
         float personagemPrincipalWidth = personagemPrincipalSprite.getWidth();
         float personagemPrincipalHeight = personagemPrincipalSprite.getHeight();
+        personagemPrincipalSprite.setY(MathUtils.clamp(personagemPrincipalSprite.getY(), 1, worldHeight - personagemPrincipalHeight));
 
-        //Vamos impedir que o personagem sai da tela para valores entre 0 e a altura do mundo
-        personagemPrincipalSprite.setY(MathUtils.clamp(personagemPrincipalSprite.getY(), 0, worldHeight - personagemPrincipalHeight));
+        // Hitbox Personagem Principal
+        PersonagemRectangle.set(personagemPrincipalSprite.getX(), personagemPrincipalSprite.getY(), personagemPrincipalWidth, personagemPrincipalHeight);
+
+        for (int i = vetorDiscos.size - 1; i >= 0; i--){
+            Sprite disco = vetorDiscos.get(i);
+            float discoWidth = disco.getWidth();
+            float discoHeight = disco.getHeight();
+
+            disco.translateX(-2f * delta);
+            discoRectangle.set(disco.getX(), disco.getY(), discoWidth, discoHeight);
+
+            if(disco.getX() < -discoWidth){
+                vetorDiscos.removeIndex(i);
+            }else if(discoRectangle.overlaps(PersonagemRectangle)){
+                vetorDiscos.removeIndex(i);
+            }
+        }
+        discoTimer += delta;
+        if (discoTimer > 1f) {
+            discoTimer = 0;
+            criarDiscos();
+        }
+    
     }
 
     public void draw(){
         ScreenUtils.clear(Color.BLACK);
         viewport.apply();
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
-
-        //Desenhar as coisas é por aqui
+        
+        // ----- Desenhar as coisas é por aqui
         spriteBatch.begin();
 
-        spriteBatch.draw(fundoExemplo, 0, 0, viewport.getWorldHeight(), viewport.getWorldWidth());
+        spriteBatch.draw(fundoExemplo, 0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
         personagemPrincipalSprite.draw(spriteBatch);
+        for (Sprite disco : vetorDiscos){
+            disco.draw(spriteBatch);
+        }
 
         spriteBatch.end();
     }
